157aa2eb25dd1f10562212b5df156ccd98743598
LeeYongYun <yj9334@naver.com>
2023-08-30 18:03:39 +0900

    세션체크 및 메소드 체크 추가

--- chat_traveling_BE/src/main/java/com/example/demo/ChatTravelingBeApplication.java
+++ chat_traveling_BE/src/main/java/com/example/demo/ChatTravelingBeApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,6 +20,12 @@ public class ChatTravelingBeApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                		HttpMethod.GET.name(),
+                		HttpMethod.POST.name(),
+                		HttpMethod.PUT.name(), 
+                		HttpMethod.DELETE.name()
+                		)
                 .allowCredentials(true);
             }
         };
--- chat_traveling_BE/src/main/java/com/example/demo/controller/TestController.java
+++ chat_traveling_BE/src/main/java/com/example/demo/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,6 +7,7 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +19,21 @@ import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class TestController {
+	@PostMapping("login")
+	public String login(HttpSession session) throws JsonProcessingException {
+		session.setAttribute("id", "yjlee"); 
+		Map<String,Object> pageSession = new HashMap<String,Object>();
+		pageSession.put("pageSession", "Y");
+		return (new ObjectMapper()).writeValueAsString(pageSession);
+	}
 	
-	@PostMapping("/hello133333")
+	@PutMapping("/hello133333")
 	public String main11(HttpServletRequest request, @RequestBody Map<String,Object> jsonData) throws JsonProcessingException { 
 		HttpSession session = request.getSession();
-		System.out.println(session.getAttribute("id"));
-		session.setAttribute("id", "yjlee"); 
-		System.out.println("hello11");
+		String id = (String) session.getAttribute("id");
+		if(id == null || id.isEmpty()) { 
+			return "";
+		}
 		ObjectMapper objectMapper = new ObjectMapper(); 
 		List<Map<String,Object>> mList = new ArrayList<Map<String,Object>>();
 		jsonData.put("chatAsw","반갑습니다");
@@ -34,6 +42,12 @@ public class TestController {
 	}
 	@GetMapping("/hello123")
 	public String main1() {  
+		System.out.println("hello???");
+		return "{[{sendChat:\"안녕하세요\",receiveChat:\"반갑습니다\"}]}";
+	}
+	
+	@PutMapping("/hello111111")
+	public String main11() {  
 		System.out.println("hello???");
 		return "{[{sendChat:\"안녕하세요\",receiveChat:\"반갑습니다\"}]}";
 	}
